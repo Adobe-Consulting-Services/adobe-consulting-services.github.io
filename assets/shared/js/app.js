c8b53ee739466d1ca2caa90d3a77fd3830e5fdda
David Gonzalez <davidjgonzalez@gmail.com>
2014-04-29 21:10:46 -0400

    #3 - iOS related bug fixes

--- assets/shared/js/app.js
+++ assets/shared/js/app.js
@@ -4,7 +4,7 @@ $.bigfoot();
 	var wrapper = $('<div class="img-wrapper"></div>');
 	$('.feature-doc img').wrap(wrapper);
 
-	$('a[href^="http"').attr('target', '_blank');
+	$("a[href^='http']").attr('target', '_blank');
 });
 
 
